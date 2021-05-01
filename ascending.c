00bd3e81c401bea9b539efdc1e430d23dd926425
ANJANA A <81566311+Anjana2002@users.noreply.github.com>
2021-05-01 20:05:07 +0530

    Add files via upload

---
+++ ascending.c
@@ -0,0 +1,33 @@
+//arranging no in ascending order
+#include<stdio.h>
+void main()
+{
+  int n,i,a[10],j,b;
+  printf("How many numbers you want to print : " );
+  scanf("%d",&n );
+  printf("Enter the numbers: ");
+  for(i=0;i<n;i++)
+  {
+    scanf("%d",&a[i]);
+  }
+
+  for(i=0;i<n-1;i++)
+  {
+    for(j=i+1;j<n;j++)
+    {
+      if(a[i]>a[j])
+      {
+        b=a[i];
+        a[i]=a[j];
+        a[j]=b;
+      }
+    }
+  }
+
+  printf("Ascending order: ");
+  for(i=0;i<n;i++)
+  {
+    printf("%d\t",a[i]);
+  }
+  printf("\n");
+}
