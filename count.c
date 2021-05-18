1c55c4e85227525fe50604fe995e0061808ea249
ANJANA A <81566311+Anjana2002@users.noreply.github.com>
2021-05-18 16:37:28 +0530

    Add files via upload

---
+++ count.c
@@ -0,0 +1,33 @@
+//count how many positive,negative & zero
+#include<stdio.h>
+int main()
+{
+  int a[50],n,i,j,pos=0,neg=0,zero=0;
+  printf("\nHow many numbers you want to enter?  ");
+  scanf("%d",&n);
+  printf("Enter the numbers: ");
+  for(i=0;i<n;i++)
+  {
+    scanf("%d",&a[i]);
+  }
+  //counting
+  for(i=0;i<n;i++)
+  {
+    if(a[i]>0)
+    {
+      pos++;
+    }
+    else if(a[i]<0)
+    {
+      neg++;
+    }
+    else
+    {
+      zero++;
+    }
+  }
+  printf("\ncount of positive numbers:%d\n",pos );
+  printf("count of negative numbers:%d\n",neg );
+  printf("count of zero:%d\n",zero );
+  printf("\n");
+}
